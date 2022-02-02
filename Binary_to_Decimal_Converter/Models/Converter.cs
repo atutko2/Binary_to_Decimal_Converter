e01d3fc471fcde34d44dcf258f7c31953b98b918
Adam Tutko <atutko@vols.utk.edu>
2022-02-02 15:22:02 -0500

    First Commit

---
+++ Binary_to_Decimal_Converter/Models/Converter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Numerics;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Binary_to_Decimal_Converter.Models
+{
+    public class Converter
+    {
+        public Converter()
+        {
+            this.ReturnVal = "";
+        }
+
+        [BindProperty]
+        public string ValueToConvert { get; set; }
+
+        public string ReturnVal { get; set; }
+
+        internal void convert( int convertType )
+        {
+
+            if (convertType == 1)
+            {
+                BigInteger conversion = new BigInteger(0);
+
+                for (int i = 0; i < ValueToConvert.Length; i++)
+                {
+                    conversion *= 2;
+                    if (ValueToConvert[i] == '1')
+                        conversion++;
+                }
+
+                ReturnVal = conversion.ToString();
+            }
+            else
+            {
+                List<int> bin = new List<int>();
+                BigInteger val = BigInteger.Parse(ValueToConvert);
+                
+
+                while (val > 0)
+                {
+                    bin.Insert(0, (int)(val % 2));
+                    val /= 2;
+                }
+
+                StringBuilder builder = new StringBuilder();
+                
+                foreach (int num in bin)
+                {
+                   builder.Append(num);
+                }
+                
+                ReturnVal = builder.ToString();
+            }
+        }
+
+    }
+}
+
+public enum Options { Binary, Decimal }
---
+++ Binary_to_Decimal_Converter/Pages/Index.cshtml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Binary_to_Decimal_Converter.Models;
+
+
+
+
+namespace Binary_to_Decimal_Converter.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty]
+        public Converter NewConverter { get; set; }
+
+        [BindProperty]
+        public string ConvertFrom { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+            NewConverter = new Converter();
+            ConvertFrom = "Binary";
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnPost()
+        {
+
+
+            if (ConvertFrom == "Binary")
+            {
+
+                for (int i = 0; i < NewConverter.ValueToConvert.Length; i++)
+                {
+                    // check the input to make sure it is just binary
+                    if (NewConverter.ValueToConvert[i] != '0' && NewConverter.ValueToConvert[i] != '1')
+                    {
+                        ModelState.AddModelError("ValueToConvert", "Value must only contain binary digits.");
+                        break;
+                    }
+                }
+
+                NewConverter.convert(1);
+            }
+            else
+            {
+                for (int i = 0; i < NewConverter.ValueToConvert.Length; i++)
+                {
+                    // check the input to make sure it is just binary
+                    if (NewConverter.ValueToConvert[i] < '0' || NewConverter.ValueToConvert[i] > '9')
+                    {
+                        ModelState.AddModelError("ValueToConvert", "Value must only contain numbers.");
+                        break;
+                    }
+                }
+                NewConverter.convert(2);
+            }
+
+
+            // if any of the input was bad, return the page with the errors
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+
+            // if the page is valid, clear any previous errors that may have been set
+            foreach (var modelValue in ModelState.Values)
+            {
+                modelValue.Errors.Clear();
+            }
+
+            return Page();
+        }
+
+    }
+}
